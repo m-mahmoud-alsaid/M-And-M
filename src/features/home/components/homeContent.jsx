b17713b60a65494766179c6647abdb763adf50d6
Mohamed Mahmoud <m.mahmoud.alsaid.official@gmail.com>
2026-06-06 19:33:22 +0300

    Build Router, move all home components to /features/home/components and create home page

---
+++ src/features/home/components/homeContent.jsx
@@ -0,0 +1,14 @@
+import Hero from '@/features/home/components/hero'
+import Services from '@/features/home/components/services'
+
+function HomeContent() {
+
+    return (
+        <>
+            <Hero />
+            <Services />
+        </>
+    )
+}
+
+export default HomeContent;
